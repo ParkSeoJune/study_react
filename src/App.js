3d090437ffb782609f94e72856db5a5fdaced628
ParkSeoJune <tjwns4122@gmail.com>
2022-11-29 17:26:34 +0900

    useReducer로 복잡한 State다루기(출석부)

--- src/App.js
+++ src/App.js
@@ -1,55 +1,79 @@
 import "./App.css";
 import React, { useReducer, useState } from "react";
+import Student from "./Student";
 
-//reducer - state를 업데이트 하는 역할(은행)
-//dispatch - state 업데이트를 위한 요구
-//action - 요구의 내용
-
-const ACTION_TYPE = {
-  deposit: "deposit",
-  withdraw: "withdraw",
+const initialState = {
+  count: 0,
+  students: [],
 };
 
 const reducer = (state, action) => {
-  console.log("reducer가 일을 합니다", state, action); // state는 money
   switch (action.type) {
-    case ACTION_TYPE.deposit:
-      return state + action.payload;
-    case ACTION_TYPE.withdraw:
-      return state - action.payload;
+    case "add-student":
+      const name = action.payload.name;
+      const newStudent = {
+        id: Date.now(),
+        name,
+        isHere: false,
+      };
+      return {
+        count: state.count + 1,
+        students: [...state.students, newStudent],
+      };
+    case "delete-student":
+      return {
+        count: state.count - 1,
+        students: state.students.filter(
+          (student) => student.id !== action.payload.id
+        ),
+      };
+    case "mark-student":
+      return {
+        count: state.count,
+        students: state.students.map((student) => {
+          if (student.id === action.payload.id) {
+            return { ...student, isHere: !student.isHere };
+          }
+          return student;
+        }),
+      };
     default:
       return state;
   }
 };
 
 function App() {
-  const [number, setNumber] = useState(0);
-  const [money, dispatch] = useReducer(reducer, 0);
+  const [name, setName] = useState("");
+  const [studentsInfo, dispatch] = useReducer(reducer, initialState);
 
   return (
     <div>
-      <h2>useReducer 은행에 오신것을 환영합니다</h2>
-      <p>잔고: {money}원</p>
+      <h1>출석부</h1>
+      <p>총 학생 수: {studentsInfo.count} </p>
       <input
-        type="number"
-        value={number}
-        onChange={(e) => setNumber(parseInt(e.target.value))}
-        step="1000"
+        type="text"
+        placeholder="이름을 입력해주세요"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
       />
       <button
         onClick={() => {
-          dispatch({ type: ACTION_TYPE.deposit, payload: number });
+          dispatch({ type: "add-student", payload: { name } });
         }}
       >
-        예금
+        추가
       </button>
-      <button
-        onClick={() => {
-          dispatch({ type: ACTION_TYPE.withdraw, payload: number });
-        }}
-      >
-        출금
-      </button>
+      {studentsInfo.students.map((student) => {
+        return (
+          <Student
+            name={student.name}
+            key={student.id}
+            dispatch={dispatch}
+            id={student.id}
+            isHere={student.isHere}
+          />
+        );
+      })}
     </div>
   );
 }
